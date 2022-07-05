f5d3aad75581180b1a1f037927f704f385b9b40f
pandu-supriyono <mail@supriyono.nl>
2022-07-05 14:52:10 +0200

    implement routing and authProvider to entrypoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import { ROUTE_MANIFEST } from './constants';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { DashboardPage } from './pages/DashboardPage';
+import { HomePage } from './pages/HomePage';
+import { LogInPage } from './pages/LogInPage';
+import { AuthProvider } from './auth/context/AuthContext';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { AuthenticatedRoute } from './auth/router/AuthenticatedRoute';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTE_MANIFEST.ROOT.path} element={<App />}>
+            <Route path={ROUTE_MANIFEST.ROOT.path} element={<HomePage />} />
+            <Route path={ROUTE_MANIFEST.HOME.path} element={<HomePage />} />
+            <Route
+              path={ROUTE_MANIFEST.DASHBOARD.path}
+              element={
+                <AuthenticatedRoute allowFrom={ROUTE_MANIFEST.HOME.path}>
+                  <DashboardPage />
+                </AuthenticatedRoute>
+              }
+            />
+            <Route path={ROUTE_MANIFEST.LOG_IN.path} element={<LogInPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
 

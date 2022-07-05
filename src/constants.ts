8c01ae389fb58b52984b4ed832f9ce585b07bf77
pandu-supriyono <mail@supriyono.nl>
2022-07-05 09:37:31 +0200

    add route manifest
    This object of constants prevents chances of using paths that don't or
    shouldn't exist in the app

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,20 @@
 export const ACCESS_TOKEN_KEY = '__unc_inc_access_token__';
+
+export const ROUTE_MANIFEST = Object.freeze({
+  ROOT: {
+    path: '/',
+    title: 'Home',
+  },
+  HOME: {
+    path: '/home',
+    title: 'Home',
+  },
+  LOG_IN: {
+    path: '/log-in',
+    title: 'Log in',
+  },
+  DASHBOARD: {
+    path: '/dashboard',
+    title: 'Dashboard',
+  },
+});
